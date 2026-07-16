a888b41398477db32b5c9172e8d7c1b1bfa9ce45
Ansari Akhtar <akhtarannsari06@gmail.com>
2026-07-17 00:23:17 +0530

    Add files via upload

---
+++ 11_greatest _number.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c;
+    cout << " Enter Three Numbers : " << endl;
+    cin >> a >> b >> c;
+    if (a > b)
+    {
+        if (a > c)
+            cout << "Greatest Number is " << a << endl;
+        else
+            cout << "Greatest Number is " << c << endl;
+    }
+    else
+    {
+        if (b > c)
+            cout << "Greatest Number is " << b << endl;
+        else
+            cout << "Greatest Number is " << c << endl;
+    }
+    return 0;
+}
